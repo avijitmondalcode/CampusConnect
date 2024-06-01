13875d42d65999f1c1499de69ab75680f909a42c
Avijit Mondal <avijitmondalcode12@gmail.com>
2024-06-02 00:17:36 +0530

    first change

---
+++ app/src/main/java/com/example/campusconnect/AppConstants.java
@@ -0,0 +1,9 @@
+package com.example.campusconnect;
+
+public class AppConstants {
+
+    public static long appId = 1886856625;
+    public static String appSign = "45a8153b1a1568eb1e56eed90f773dca502eec577ea940c8f4521657e0b04dd8";
+
+
+}
